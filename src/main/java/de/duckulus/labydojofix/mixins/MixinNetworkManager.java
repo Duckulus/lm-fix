51099479cbfca24ae5cb3d22d47daf12e824470e
aminh <amin.haddou@outlook.de>
2023-11-26 02:37:45 +0100

    fix typo

--- src/main/java/de/duckulus/labydojofix/mixins/MixinNetworkManager.java
+++ src/main/java/de/duckulus/labydojofix/mixins/MixinNetworkManager.java
@@ -1,9 +1,9 @@
 package de.duckulus.labydojofix.mixins;
 
+import de.duckulus.labydojofix.LabyDojoFix;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C17PacketCustomPayload;
-import de.duckulus.labydojofix.LabyDojoFix;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
